a6df22983564e3798b7a380f4b5cd1b260638799
upkardroid <mail.upkarsingh@gmail.com>
2018-10-26 17:54:39 +0530

    Refresh removing filetype from .gitignore file.

--- public/src/app.js
+++
@@ -1,7 +0,0 @@
-console.log("app is runnning");
-
-
-var template = <h1>this is runnig the react now! wohoooooo </h1>;
-var appRoot = document.getElementById("app");
-
-ReactDOM.render(template,appRoot);
