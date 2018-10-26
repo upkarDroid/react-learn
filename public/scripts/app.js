6676c43c527bdbde24c76970b3c16bae43b1d597
upkardroid <mail.upkarsingh@gmail.com>
2018-10-26 18:23:27 +0530

    Refresh removing filetype from .gitignore file.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,10 +2,61 @@
 
 console.log("app is runnning");
 
+// var data = {
+//     name:"upkar",
+//     age:"30",
+//     address:"bangalore"
+// }
+
+var data = {
+    name: "ritu",
+    age: "29",
+    address: "pune"
+};
+
+var app = {
+    title: "this is some react shit"
+};
+
+function getAddress(obj) {
+    return obj.address ? React.createElement(
+        "li",
+        null,
+        "address : ",
+        obj.address
+    ) : null;
+}
+
 var template = React.createElement(
-  "h1",
-  null,
-  "this is runnig the react now! wohoooooo "
+    "div",
+    null,
+    app.title && React.createElement(
+        "h1",
+        null,
+        app.title
+    ),
+    React.createElement(
+        "ul",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "UserName : ",
+            data.name
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Age : ",
+            data.age
+        ),
+        getAddress(data)
+    ),
+    React.createElement(
+        "p",
+        null,
+        "lorem ipsum"
+    )
 );
 var appRoot = document.getElementById("app");
 
--- public/src/app.js
+++ public/src/app.js
@@ -1,7 +1,38 @@
 console.log("app is runnning");
 
+// var data = {
+//     name:"upkar",
+//     age:"30",
+//     address:"bangalore"
+// }
 
-var template = <h1>this is runnig the react now! wohoooooo </h1>;
+var data = {
+    name:"ritu",
+    age:"29",
+    address:"pune"
+}
+
+var app={
+    title:"this is some react shit"
+}
+
+function getAddress(obj) {
+return obj.address ? <li>address : {obj.address}</li> : null;
+}
+
+var template = (
+<div>
+
+    {app.title && <h1>{app.title}</h1>}
+    <ul>
+    <li>UserName : {data.name}</li>
+    <li>Age : {data.age}</li>
+    {getAddress(data)}
+    </ul>
+    <p>lorem ipsum</p>
+</div>
+    
+);
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template,appRoot);
